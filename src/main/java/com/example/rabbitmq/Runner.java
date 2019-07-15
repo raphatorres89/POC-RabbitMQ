5248157474aaafa51e0e9019a1b36947bb5fb72f
Raphael Torres <rapha.oo.torres@gmail.com>
2019-07-15 12:37:01 -0300

    Integração do Spring com RabbitMQ

---
+++ src/main/java/com/example/rabbitmq/Runner.java
@@ -0,0 +1,28 @@
+package com.example.rabbitmq;
+
+import com.example.rabbitmq.configs.RabbitMQConfig;
+import com.example.rabbitmq.receivers.Receiver;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
+
+@Component
+public class Runner implements CommandLineRunner {
+
+    private final RabbitTemplate rabbitTemplate;
+    private final Receiver receiver;
+
+    public Runner(RabbitTemplate rabbitTemplate, Receiver receiver) {
+        this.rabbitTemplate = rabbitTemplate;
+        this.receiver = receiver;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Sending message...");
+        rabbitTemplate.convertAndSend(RabbitMQConfig.topicExchangeName, "foo.bar.baz", "Hello from RabbitMQ");
+        receiver.getLatch().await(10000, TimeUnit.MILLISECONDS);
+    }
+}
---
+++ src/main/java/com/example/rabbitmq/configs/RabbitMQConfig.java
@@ -0,0 +1,47 @@
+package com.example.rabbitmq.configs;
+
+import com.example.rabbitmq.receivers.Receiver;
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    public static final String topicExchangeName = "spring-boot-extchange";
+
+    public static final String queueName = "spring-boot";
+
+    @Bean
+    Queue queue() {
+        return new Queue(queueName, false);
+    }
+
+    @Bean
+    TopicExchange exchange() {
+        return new TopicExchange(topicExchangeName);
+    }
+
+    @Bean
+    Binding binding(Queue queue, TopicExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with("foo.bar.#");
+    }
+
+    @Bean
+    SimpleMessageListenerContainer container(ConnectionFactory connectionFactory, MessageListenerAdapter listenerAdapter) {
+        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+        container.setQueueNames(queueName);
+        container.setMessageListener(listenerAdapter);
+        return container;
+    }
+
+    @Bean
+    MessageListenerAdapter listenerAdapter(Receiver receiver) {
+        return new MessageListenerAdapter(receiver, "receiveMessage");
+    }
+
+}
